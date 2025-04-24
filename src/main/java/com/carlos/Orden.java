db1df7559909f65a448fc43fdfbf167d2687433e
Benjamin <carlosbpac@gmail.com>
2025-04-24 16:40:09 -0600

    Add AddType enum and update Orden class to use typed properties for plato, bebida, and extras

--- src/main/java/com/carlos/Orden.java
+++ src/main/java/com/carlos/Orden.java
@@ -3,9 +3,10 @@ package com.carlos;
 import java.util.List;
 
 public class Orden {
-    private String plato;
-    private String bebida;
-    private List<String> extras;
+    private PlatoType plato;
+    private AddType acompañamiento;
+    private BebidaType bebida;
+    private List<ExtraType> extras;
 
     public Orden(){
         this.extras = List.of();
@@ -14,52 +15,64 @@ public class Orden {
     /**
      * @return the plato
      */
-    public String getPlato() {
+    public PlatoType getPlato() {
         return plato;
     }
 
     /**
      * @param plato the plato to set
      */
-    public void setPlato(String plato) {
+    public void setPlato(PlatoType plato) {
         this.plato = plato;
     }
 
     /**
      * @return the bebida
      */
-    public String getBebida() {
+    public BebidaType getBebida() {
         return bebida;
     }
 
     /**
      * @param bebida the bebida to set
      */
-    public void setBebida(String bebida) {
+    public void setBebida(BebidaType bebida) {
         this.bebida = bebida;
     }
 
     /**
      * @return the extras
      */
-    public List<String> getExtras() {
+    public List<ExtraType> getExtras() {
         return extras;
     }
 
     /**
      * @param extras the extras to set
      */
-    public void setExtras(List<String> extras) {
+    public void setExtras(List<ExtraType> extras) {
         this.extras = extras;
     }
 
     /**
      * @param extra the extra to add
      */
-    public void addExtra(String extra) {
+    public void addExtra(ExtraType extra) {
         this.extras.add(extra);
     }
 
-    
+    /**
+     * @return the acompañamiento
+     */
+    public AddType getAcompañamiento() {
+        return acompañamiento;
+    }
+
+    /**
+     * @param acompañamiento the acompañamiento to set
+     */
+    public void setAcompañamiento(AddType acompañamiento) {
+        this.acompañamiento = acompañamiento;
+    }
 
 }
